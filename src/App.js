4943226bc6a74751286a3435aae922144f741a79
Małgorzata Olak <m.olak@outlook.com>
2018-10-19 15:40:18 +0200

    srcset images

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import React, { Component } from 'react';
-import TitleView from './views/TitleView';
-import styled from 'styled-components';
-import './App.css';
+import React, { Component } from "react";
+import TitleView from "./views/TitleView";
+import styled from "styled-components";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-        <AppContainer>
-           <TitleView/>
-        </AppContainer>
+      <AppContainer>
+        <TitleView />
+      </AppContainer>
     );
   }
 }
 
 export default App;
 
-const AppContainer=styled.div`
-width:100%;
-height:100%;
+const AppContainer = styled.div`
+  width: 100%;
+  height: 100%;
 
+  img {
+    width: 100%;
+  }
 `;
--- src/views/TitleView/index.js
+++ src/views/TitleView/index.js
@@ -1,25 +1,30 @@
-import React, { Component, Fragment } from 'react';
-import styled from 'styled-components';
-import title2 from './assets/title1.jpg';
+import React, { Component, Fragment } from "react";
+import styled from "styled-components";
+import mainbig from "./assets/mainbig.jpg";
+import main600 from "./assets/main600.jpg";
+import main900 from "./assets/main900.jpg";
+import main1100 from "./assets/main1100.jpg";
+import main1700 from "./assets/main1700.jpg";
 
-class TitleView extends Component{
-    constructor(props){
-        super(props);
-    }
+class TitleView extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-
-    render(){
-        return(<div>
-            <MainImg/>
-        </div>);
-    }
-};
+  render() {
+    return (
+      <div>
+        <MainImg />
+      </div>
+    );
+  }
+}
 
 export default TitleView;
 
-
 const MainImg = styled.img.attrs({
-    src: `${title2}`
+  src: `${mainbig}`,
+  srcSet: `${main1700} 1600w, ${main1100} 1000w, ${main900} 800w, ${main600} 600w`
 })`
-width:100%;
+  height: auto;
 `;
