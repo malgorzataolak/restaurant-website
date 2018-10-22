0a9fb53f0ef442e0b8751289efaefdd4860c0ef1
Małgorzata Olak <m.olak@outlook.com>
2018-10-22 16:52:59 +0200

    Slider in progress

--- src/views/TitleView/index.js
+++ src/views/TitleView/index.js
@@ -5,26 +5,128 @@ import main600 from "./assets/main600.jpg";
 import main900 from "./assets/main900.jpg";
 import main1100 from "./assets/main1100.jpg";
 import main1700 from "./assets/main1700.jpg";
+import sushi1 from "./assets/Sushi1.png";
+import sushi2 from "./assets/Sushi2.png";
+import sushi3 from "./assets/Sushi3.png";
 
 class TitleView extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      images: [sushi1, sushi2, sushi3],
+      currentIndex: 0,
+      translateValue: 0
+    };
+    this.nextPicture = this.nextPicture.bind(this);
+    this.previousPicture = this.previousPicture.bind(this);
+    this.getCurrentWidth = this.getCurrentWidth.bind(this);
   }
 
+  nextPicture() {
+    if (this.state.currentIndex === this.state.images.length - 1) {
+      return this.setState({
+        currentIndex: 0,
+        translateValue: 0
+      });
+    }
+    this.setState(prevState => ({
+      currentIndex: prevState.currentIndex + 1,
+      translateValue: prevState.translateValue + -this.getCurrentWidth()
+    }));
+  }
+
+  previousPicture() {
+    if (this.state.currentIndex === 0) return;
+
+    this.setState(prevState => ({
+      currentIndex: prevState.currentIndex - 1,
+      translateValue: prevState.translateValue + this.getCurrentWidth()
+    }));
+  }
+
+  getCurrentWidth() {
+    console.log("width", document.getElementById("slider").clientWidth);
+    return document.getElementById("slider").clientWidth;
+  }
   render() {
+    console.log("currentState", this.state);
     return (
-      <div>
-        <MainImg />
-      </div>
+      <Container>
+        <ArrowLeft onClick={this.previousPicture} />
+
+        <Slider
+          id={"slider"}
+          image={this.state.images[this.state.currentIndex]}
+        />
+
+        <ArrowRight onClick={this.nextPicture} />
+      </Container>
     );
   }
 }
 
 export default TitleView;
+const SliderWrapper = styled.div`
+  height: 100%;
+  width: 100%;
+  transform: ${props =>
+    props.value ? `translateX(${props.value}px)` : undefined};
+  transition: transform ease-out 0.45s;
+`;
+
+const Container = styled.div`
+  position: relative;
+  width: 100%;
+  height: 770px;
+  overflow: hidden;
+  white-space: nowrap;
+`;
+
+const Slider = styled.div`
+  height: 100%;
+  width: 100%;
+  background-image: ${props =>
+    props.image ? `url(${props.image})` : undefined};
+  background-size: cover;
+`;
 
 const MainImg = styled.img.attrs({
   src: `${mainbig}`,
-  srcSet: `${main1700} 1600w, ${main1100} 1000w, ${main900} 800w, ${main600} 600w`
+  srcSet: `${sushi1} 1600w, ${main1100} 1000w, ${main900} 800w, ${main600} 600w`
 })`
   height: auto;
+`;
+
+const ArrowRight = styled.div`
+  opacity: 0.5;
+  position: absolute;
+  z-index: 100;
+  top: 50%;
+  right: 1rem;
+  width: 0;
+  height: 0;
+  border-style: solid;
+  border-width: 25px 0 25px 30px;
+  border-color: transparent transparent transparent #ffffff;
+  &:hover {
+    opacity: 0.9;
+    cursor: pointer;
+  }
+`;
+
+const ArrowLeft = styled.div`
+  opacity: 0.5;
+  position: absolute;
+  z-index: 100;
+  top: 50%;
+  left: 1rem;
+  width: 0;
+  height: 0;
+  border-style: solid;
+  border-width: 25px 30px 25px 0;
+  border-color: transparent #ffffff transparent transparent;
+  &:hover {
+    opacity: 0.9;
+    cursor: pointer;
+  }
 `;
