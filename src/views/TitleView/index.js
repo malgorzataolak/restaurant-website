18cb31d6dc8c0713b6fe005354dd88aef43e5063
Małgorzata Olak <m.olak@outlook.com>
2018-10-23 16:13:28 +0200

    Scale arrows, smaller pictures added

--- src/views/TitleView/index.js
+++ src/views/TitleView/index.js
@@ -1,20 +1,22 @@
 import React, { Component, Fragment } from "react";
 import styled from "styled-components";
 import posed, { PoseGroup } from "react-pose";
-import mainbig from "./assets/mainbig.jpg";
-import main600 from "./assets/main600.jpg";
-import main900 from "./assets/main900.jpg";
-import main1100 from "./assets/main1100.jpg";
-import main1700 from "./assets/main1700.jpg";
 import sushi1 from "./assets/Sushi1.png";
+import sushi1_1100 from "./assets/Sushi1_1100.png";
 import sushi2 from "./assets/Sushi2.png";
+import sushi2_1100 from "./assets/Sushi2_1100.png";
 import sushi3 from "./assets/Sushi3.png";
+import sushi3_1100 from "./assets/Sushi3_1100.png";
 
 class TitleView extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      images: [sushi1, sushi2, sushi3],
+      images: [
+        { big: sushi1, xl: sushi1_1100 },
+        { big: sushi2, xl: sushi2_1100 },
+        { big: sushi3, xl: sushi3_1100 }
+      ],
       currentIndex: 0,
       translateValue: 0,
       leftVisible: false
@@ -96,33 +98,15 @@ const SliderWrapper = styled.div`
   transition: transform ease-out 0.45s;
 `;
 
-const Container = styled.div`
-  position: relative;
-  width: 100%;
-  height: 100%;
-  overflow: hidden;
-  white-space: nowrap;
-  -webkit-touch-callout: none; /* iOS Safari */
-  -webkit-user-select: none; /* Safari */
-  -khtml-user-select: none; /* Konqueror HTML */
-  -moz-user-select: none; /* Firefox */
-  -ms-user-select: none; /* Internet Explorer/Edge */
-  user-select: none; /* Non-prefixed version, currently
-                                  supported by Chrome and Opera */
-`;
-
 const Slider = styled.img.attrs({
-  src: props => props.image
+  //src: props => props.image.big,
+  srcSet: props =>
+    props.image
+      ? `${props.image.big} 1600w, ${props.image.xl} 1100w `
+      : "undefined"
 })`
   height: 100%;
   width: 100%;
-`;
-
-const MainImg = styled.img.attrs({
-  src: `${mainbig}`,
-  srcSet: `${sushi1} 1600w, ${main1100} 1000w, ${main900} 800w, ${main600} 600w`
-})`
-  height: auto;
 `;
 
 const ArrowRight = styled.div`
@@ -136,6 +120,7 @@ const ArrowRight = styled.div`
   border-style: solid;
   border-width: 25px 0 25px 30px;
   border-color: transparent transparent transparent #ffffff;
+  transform: translate(-50%, -50%);
   &:hover {
     opacity: 0.9;
     cursor: pointer;
@@ -153,6 +138,8 @@ const ArrowLeft = styled.div`
   border-style: solid;
   border-width: 25px 30px 25px 0;
   border-color: transparent #ffffff transparent transparent;
+  transform: translate(50%, -50%);
+
   &:hover {
     opacity: 0.9;
     cursor: pointer;
@@ -163,4 +150,29 @@ const ArrowLeft = styled.div`
 
     display:none;
     `};
+`;
+
+const Container = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  white-space: nowrap;
+  -webkit-touch-callout: none; /* iOS Safari */
+  -webkit-user-select: none; /* Safari */
+  -khtml-user-select: none; /* Konqueror HTML */
+  -moz-user-select: none; /* Firefox */
+  -ms-user-select: none; /* Internet Explorer/Edge */
+  user-select: none; /* Non-prefixed version, currently
+                                  supported by Chrome and Opera */
+
+  @media screen and (max-width: 1100px) {
+    ${ArrowLeft} {
+      border-width: 15px 20px 15px 0;
+    }
+
+    ${ArrowRight} {
+      border-width: 15px 0 15px 20px;
+    }
+  }
 `;
