7e194909d17f41612c5849eafb79139517b15ccb
Małgorzata Olak <m.olak@outlook.com>
2018-12-11 16:50:15 +0100

    WeekDays for Calendar component added

--- src/views/Reservations/Calendar/index.js
+++ src/views/Reservations/Calendar/index.js
@@ -1,14 +1,64 @@
 import React, { Component, Fragment } from "react";
+import moment from "moment";
 import styled from "styled-components";
+
+const WeekDays = () => {
+  let weeks = [];
+  for (let i = 1; i <= 7; i++) {
+    console.log(
+      "weekdays",
+      moment()
+        .day(i)
+        .format("ddd")
+    );
+    weeks.push(
+      <WeekDay key={moment().day(i)}>
+        {moment()
+          .day(i)
+          .format("ddd")}
+      </WeekDay>
+    );
+  }
+
+  return weeks;
+};
 
 class Calendar extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      selectedDate: moment()
+    };
+    this.selectDate = this.selectDate.bind(this);
+    this.getPreviousMonth = this.getPreviousMonth.bind(this);
+    this.getNextMonth = this.getNextMonth.bind(this);
   }
 
+  selectDate() {}
+
+  getPreviousMonth() {}
+
+  getNextMonth() {}
+
   render() {
-    return <div>Calendar Placeholder</div>;
+    return (
+      <CalendarContainer>
+        <SelectedDate>{moment().format("DD-MM-YYYY")}</SelectedDate>
+        <WeekDays />
+      </CalendarContainer>
+    );
   }
 }
 
 export default Calendar;
+
+const WeekDay = styled.div`
+  display: inline-block;
+  text-align: center;
+  text-transform: uppercase;
+  padding: 5px;
+`;
+
+const CalendarContainer = styled.div``;
+
+const SelectedDate = styled.div``;
